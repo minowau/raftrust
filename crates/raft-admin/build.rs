56a7c0745e38ce99d5c6d5e5bc95ae213093b55e
Louis-Philip Marcoux <louisphilipmarcoux@gmail.com>
2026-04-02 15:05:30 -0400

    Fix cargo fmt in build.rs files and add README to all crates for crates.io

--- crates/raft-admin/build.rs
+++ crates/raft-admin/build.rs
@@ -2,9 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
-        .compile_protos(
-            &["proto/kv.proto", "proto/admin.proto"],
-            &["proto"],
-        )?;
+        .compile_protos(&["proto/kv.proto", "proto/admin.proto"], &["proto"])?;
     Ok(())
 }
